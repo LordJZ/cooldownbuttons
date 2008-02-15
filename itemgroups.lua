b09f549e6674bfff3651a3d2d312df7fe60f11f2
dodge <dodge@927dd702-3d61-4a66-ac2a-2d3d9f3a112c>
2008-02-15 00:18:45 +0000

    CoolDownButtons: added Protection Potion IDs to "Other Potions" Group :)
    git-svn-id: svn://svn.wowace.com/wow/cooldownbuttons/mainline/tmp/trunk/CoolDownButtons@21 927dd702-3d61-4a66-ac2a-2d3d9f3a112c

--- itemgroups.lua
+++ itemgroups.lua
@@ -6,24 +6,27 @@ CoolDownButtons.itemgroups = {}
 CoolDownButtons.itemgroups[L["Healing Potions"]] = {
     texture = "Interface\\Icons\\INV_Potion_131",
     ids = { 32947, 33934, 4596,  1710,  929,  858, 17348,
-            13446, 118,   22829, 17349, 3928, 28100, 32905 },
+            13446, 118,   22829, 17349, 3928, 28100, 32905, },
 }
 
 CoolDownButtons.itemgroups[L["Mana Potions"]] = {
     texture = "Interface\\Icons\\INV_Potion_137",
     ids = { 32948, 33935, 31677, 6149,  3385,  17351, 13444,
-            3827,  2455,  22832, 17352, 13443, 28101, 32902 },
+            3827,  2455,  22832, 17352, 13443, 28101, 32902, },
 }
 
 CoolDownButtons.itemgroups[L["Other Potions"]] = {
     texture = "Interface\\Icons\\INV_Potion_47",
     ids = { 18253, 2456, 9144, 22850, 34440, 5633,
-            13442, 5631, 12190, 20002, 22836 },
+            13442, 5631, 12190, 20002, 22836, 6048,
+            6052, 22846, 22844, 22844, 22847, 22842,
+            22841, 22845, 6051, 13459, 13458, 13456,
+            13457, 13461, 6050, 6049, },
 }
 
 CoolDownButtons.itemgroups[L["Drums (Leatherworking)"]] = {
     texture = "Interface\\Icons\\INV_Misc_Drum_02",
-    ids = { 29529, 29532, 29531, 29530, 29528 },
+    ids = { 29529, 29532, 29531, 29530, 29528, },
 }
 
 CoolDownButtons.itemgroups[L["Healthstone"]] = {
