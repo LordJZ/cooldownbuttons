2b76bdd41196a051a099919790235356ab5037c4
dodge <dodge@927dd702-3d61-4a66-ac2a-2d3d9f3a112c>
2008-02-17 11:15:19 +0000

    CoolDownButtons: - added some translations (currently just Spellnames for Cooldown grouping, i hope it works)
    git-svn-id: svn://svn.wowace.com/wow/cooldownbuttons/mainline/tmp/trunk/CoolDownButtons@25 927dd702-3d61-4a66-ac2a-2d3d9f3a112c

---
+++ locales/esES.lua
@@ -0,0 +1,91 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CoolDown Buttons","esES")
+
+-- core.lua
+L["Click to Move"] = true
+L["RemainingCoolDown"] = "Cooldown on $spell active for $time."
+
+L["Spellgroup: Shocks"] = true
+L["Earth Shock"] = "Choque de tierra"
+L["Flame Shock"] = "Choque de llamas"
+L["Frost Shock"] = "Choque de Escarcha"
+L["Spellgroup: Traps"] = true
+L["Freezing Trap"] = "Trampa congelante"
+L["Frost Trap"] = "Trampa de Escarcha"
+L["Immolation Trap"] = "Trampa de inmolación"
+L["Snake Trap"] = "Trampa con culebras"
+L["Explosive Trap"] = "Trampa explosiva"
+
+
+-- config.lua
+L["Display Settings"] = true
+L["Direction"] = true
+L["Direction from Anchor"] = true
+
+
+L["Show Anchor"] = true
+L["Toggle showing Anchor."] = true
+L["Max Buttons"] = true
+L["Maximal number of Buttons to display."] = true
+L["Button Scale"] = true
+L["Button scaling, this lets you enlarge or shrink your Buttons."] = true
+L["Button Alpha"] = true
+L["Icon alpha value, this lets you change the transparency of the Button."] = true
+
+L["Posting Settings"] = true
+L["Post to:"] = true
+L["Enable Chatpost"] = true
+L["Toggle posting to Chat."] = true
+L["Set the Text to post."] = true
+
+L["Message Settings"] = true
+L["Use default Message"] = true
+L["Toggle posting the default Message."] = true
+L["Set the Text to post."] = true
+L["The default message is: |cFFFFFFFF$RemainingCoolDown|r"] = true
+L["Use |cFFFFFFFF$spell|r for spell name and |cFFFFFFFF$time|r for cooldowntime."] = true
+L["If \'|cFFFFFFFF$defaultmsg|r\' is disabled use the following Text"] = true
+
+L["Up"]    = true
+L["Down"]  = true
+L["Left"]  = true
+L["Right"] = true
+
+L["Default Chatframe"] = true
+L["Say"]     = true
+L["Party"]   = true
+L["Raid"]    = true
+L["Guild"]   = true
+L["Officer"] = true
+L["Emote"] = true
+L["Raidwarning"] = true
+L["Battleground"] = true
+L["Yell"] = true
+L["Custom Channel"] = true
+L["Note: Click on a Cooldown Button to post the remaining time to the above selectet Chats."] = true
+
+L["Default"] = true
+L["Char:"] = true
+L["Realm:"] = true
+L["Class:"] = true
+L["Profiles"] = true
+L["Manage Profiles"] = true
+L["You can change the active profile of CoolDown Buttons, so you can have different settings for every character"] = true
+L["Reset the current profile back to its default values, in case your configuration is broken, or you simply want to start over."] = true
+L["Reset Profile"] = true
+L["Reset the current profile to the default"] = true
+L["You can create a new profile by entering a new name in the editbox, or choosing one of the already exisiting profiles."] = true
+L["New"] = true
+L["Create a new empty profile."] = true
+L["Current"] = true
+L["Select one of your currently available profiles."] = true
+L["Delete existing and unused profiles from the database"] = true
+L["Delete a Profile"] = true
+L["Deletes a profile from the database."] = true
+L["Are you sure you want to delete the selected profile?"] = true
+
+-- itemgroups.lua
+L["Healing Potions"] = true
+L["Mana Potions"] = true
+L["Other Potions"] = true
+L["Drums (Leatherworking)"] = true
+L["Healthstone"] = true
